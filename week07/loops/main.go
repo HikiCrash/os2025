7b7d9986aac1226018690dce7af5d1a45bc30932
HikiCrash <csh02127@naver.com>
2025-10-27 11:53:03 +0900

    functions/main.go v0.3 package

--- week07/loops/main.go
+++ week07/loops/main.go
@@ -74,41 +74,41 @@
 // }
 
 // 5
-// package main
+package main
 
-// import (
-// 	"bufio"
-// 	"fmt"
-// 	"log"
-// 	"os"
-// 	"strconv"
-// 	"strings"
-// )
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
 
-// func main() {
-// 	// shadowing
-// 	// var fmt string = "code"
-// 	// var int int = 7
-// 	// var k int = 11
-// 	// fmt.Println(int)
+func main() {
+	// shadowing
+	// var fmt string = "code"
+	// var int int = 7
+	// var k int = 11
+	// fmt.Println(int)
 
-// 	r := bufio.NewReader(os.Stdin)
-// 	i, err := r.ReadString('\n')
-// 	//fmt.Println(err)
-// 	if err != nil {
-// 		log.Fatal(err) // report the error and exit the program
-// 	}
+	r := bufio.NewReader(os.Stdin)
+	i, err := r.ReadString('\n')
+	//fmt.Println(err)
+	if err != nil {
+		log.Fatal(err) // report the error and exit the program
+	}
 
-// 	i = strings.TrimSpace(i)
-// 	score, err := strconv.ParseFloat(i, 64)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
+	i = strings.TrimSpace(i)
+	score, err := strconv.ParseFloat(i, 64)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-// 	if score >= 60 {
-// 		fmt.Println("Pass")
-// 	} else {
-// 		fmt.Println("Fail")
-// 	}
-// 	fmt.Println(score)
-// }
+	if score >= 60 {
+		fmt.Println("Pass")
+	} else {
+		fmt.Println("Fail")
+	}
+	fmt.Println(score)
+}
