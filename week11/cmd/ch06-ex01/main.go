5766e5d71ddfafee1a178badb6574ea9e3651602
HikiCrash <csh02127@naver.com>
2025-11-10 11:37:14 +0900

    week11 slice v0.4

--- week11/cmd/ch06-ex01/main.go
+++ week11/cmd/ch06-ex01/main.go
@@ -3,11 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	subjects := []string{"Go", "Javascript", "Python", "Linux"}
-	subjectsSlice := subjects[1:3] // slicing
+	subjects := [4]string{"Go", "Javascript", "Python", "Linux"}
+	subjectsSlice := subjects[:3] // slicin
+	// subjects[0] = "Java"
+	subjectsSlice[0] = "java"
+
 	for _, subject := range subjects {
 		fmt.Println(subject)
 	}
+
 	fmt.Println("==========")
 	for i := 0; i < len(subjectsSlice); i++ {
 		fmt.Println(subjectsSlice[i])
