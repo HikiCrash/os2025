3d20c82f125ac804339722dc78433bc91dcef3f0
HikiCrash <csh02127@naver.com>
2025-11-17 11:41:45 +0900

    week12 struct v0.2

--- week12/cmd/ch08-ex01/main.go
+++ week12/cmd/ch08-ex01/main.go
@@ -14,6 +14,11 @@ func applyPrice(s *subscriber) {
 
 func main() {
 	var s1 subscriber
+	var p *subscriber = &s1
+
 	applyPrice(&s1)
 	fmt.Println(s1.name, s1.price)
+	// fmt.Println(*p.price) 에러
+	fmt.Println((*p).price)
+	fmt.Println(p.price)
 }
